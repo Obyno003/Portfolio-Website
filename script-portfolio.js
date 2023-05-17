d8258552d3f5f01d586a4cd047d4a09236d33baf
Obyno003 <ogbonnaemmanuel003@gmail.com>
2023-05-17 23:04:21 +0100

    first Commit

---
+++ script-portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    Strings: ["Frontend Web Developer", "UI/UX Designer", "Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
